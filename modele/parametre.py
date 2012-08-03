08779822d160b54dfe804cba04052099f54e201d
Hervé BERAUD <herveberaud.pro@gmail.com>
2012-08-03 17:05:52 +0200

    modif

---
+++ modele/parametre.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+File: parametre.py
+Author: Hervé Beraud
+Description: modele de données des parameters
+Date 03/08/2012
+Version 1.0
+CopyRight Hervé Beraud
+"""
+
+class Parametre:
+  """
+  Class Parameters
+  Modele representation of HTTP REQUEST PARAMETERS
+  """
+
+  def __init__(self):
+    """
+    Comments
+    """
+
+if __name__ == '__main__':
+  test = Parametre()
+
