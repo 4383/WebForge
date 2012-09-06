a748851dc421f21287bce12ddaccfc2b3e49ff08
Hervé BERAUD <herveberaud.pro@gmail.com>
2012-09-06 17:24:25 +0200

    [MENUBAR][EVO] add items to menubar

--- vue/menubar.py
+++ vue/menubar.py
@@ -17,20 +17,32 @@ class Menubar():
         Construteur du menu
         """
         self.menu = Menu(fenetre)
-        self.menu.add_command(label="test")
+        self.set_fichier()
+        self.set_affichage()
+        self.set_aide()
         fenetre.config(menu=self.menu)
-        #self.frame_top = Frame(fenetre, borderwidth=1, relief='groove', width=100)
-        #self.frame_top.pack( side='top', fill='both')
-        #self.titre = Label(self.frame_top, text=GT_('Url'), font=(20))
-        #self.titre.pack()
-        #self.img = PhotoImage(file="%slogo.gif" % RESSOURCES_PATH)
-        #self.logo = Label(self.frame_top, image=self.img)
-        #self.logo.pack(pady=(0, 20), side="left")
-        #self.brand = Label(self.frame_top, text=GT_('WebForge %s' % VERSION), font=(20))
-        #self.brand.pack(pady=(10, 20), padx=(0, 20), side="left")
-        #self.txt_recherche = Entry(self.frame_top, font=(22))
-        #self.txt_recherche.pack(pady=(0, 20), expand='yes', fill='both', side='left')
-        #self.btn_go = Button(self.frame_top, text="GO")
-        #self.btn_go.pack(pady=(0, 20), side='right')
-        #self.btn_erase = Button(self.frame_top, text="X")
-        #self.btn_erase.pack(pady=(0, 20), side='right')
+
+    def set_fichier(self):
+        """
+        Create the "Fichier" menu and sub-menu
+        """
+        self.fichier = Menu(self.menu, tearoff=0)
+        self.fichier.add_command(label=GT_("Quitter"))
+        self.menu.add_cascade(label=GT_("Fichier"), menu=self.fichier)
+
+    def set_affichage(self):
+        """
+        Create the "Affichage" menu and sub-menu
+        """
+        self.affichage = Menu(self.menu, tearoff=0)
+        self.affichage.add_command(label=GT_("Paramêtres"))
+        self.affichage.add_command(label=GT_("Entêtes"))
+        self.menu.add_cascade(label=GT_("Affichage"), menu=self.affichage)
+
+    def set_aide(self):
+        """
+        Create the "Fichier" menu and sub-menu
+        """
+        self.aide = Menu(self.menu, tearoff=0)
+        self.aide.add_command(label=GT_("A propos"))
+        self.menu.add_cascade(label=GT_("Aide"), menu=self.aide)
